d6f622c9368d5878709690e92e67d1c1d70370e6
Yonghyun Kwon <yonghyunkwon98@gmail.com>
2023-10-09 22:04:33 +0800

    feat: TAS-17 reset password

--- backend/src/user/exceptions.py
+++ backend/src/user/exceptions.py
@@ -1,5 +1,12 @@
 from fastapi import HTTPException, status
 
+
+class UserNotFoundException(HTTPException):
+    def __init__(self, email: str):
+        self.status_code = status.HTTP_400_BAD_REQUEST
+        self.detail = f"User {email} has not been found."
+        
+
 class FriendUsernameNotFoundException(HTTPException):
     def __init__(self, friend_username: str):
         self.status_code = status.HTTP_400_BAD_REQUEST
