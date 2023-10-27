da6177a30aaeb2417efd6c9de9b75a93f4b713de
Yonghyun Kwon <yonghyunkwon98@gmail.com>
2023-10-27 16:25:01 +0800

    feat:TAS-23 created bottom navigation bar

---
+++ frontend/screens/RecordsScreen.js
@@ -0,0 +1,22 @@
+import {
+  TouchableOpacity,
+  Text,
+  View,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+
+import React from "react";
+
+const RecordsScreen = () => {
+  return (
+    <View>
+      <Text>RecordsScreen</Text>
+    </View>
+  );
+};
+
+export default RecordsScreen;
